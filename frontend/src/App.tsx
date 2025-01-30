762d808f5e25cc430fd9deb475dda6e5ab417528
waditariq <tariqalt300@gmail.com>
2025-01-30 13:04:02 -0800

    Commit 3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Hello</h1>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign in</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
   )
 }
