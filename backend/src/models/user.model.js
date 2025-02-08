ae7c5284def15b1917e8483dc4d3844df44d1361
waditariq <tariqalt300@gmail.com>
2025-02-07 19:19:50 -0800

    Commit #4

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+    fullname: {
+        type: String,
+        required: true
+    },
+    imageUri: {
+        type: String,
+        required: true,
+
+    },
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, 
+{ timestamps: true }
+);
+// createdAt, updatedAt
