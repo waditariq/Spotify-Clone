dccb671ac40ea210d4471129d06b971d6e63ebbd
waditariq <tariqalt300@gmail.com>
2025-02-21 13:20:04 -0800

    Commit #8

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,34 @@
-export const createSong = (req, res) => {
-    res.send("Admin route with the GET method");
+import {Song} from '../models/song.model.js'
+export const createSong = async (req, res, next) => {
+    try {
+        if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+            return res.status(400).json({ message: "Please upload all files " });
+        }
+
+        const {title, artist, albumId, duration} = req.body
+        const audioFile = req.files.audioFile
+        const imageFile = req.files.imageFile
+
+        const song = new Song({
+            title,
+            artist,
+            audioUrl,
+            imageUrl,
+            duration,
+            albumId: albumId || null
+        })
+
+        await song.save()
+
+        // If song belongs to an album, update the album's songs array
+        if(albumId){
+            await albumId.findByIdAndUpdate(albumId, {
+                $push: {songs: song._id}
+            })
+        }
+        res.status(201).json(song)
+    } catch (error) {
+        console.log("Error in createSong", error);
+        next(error);
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
+import fileUpload from 'express-fileupload';
+import path from "path";
 
 import { connectDB } from './lib/db.js';
 
@@ -11,14 +13,24 @@ import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
 
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // to parse req.body
+app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits:{
+        fileSize: 10* 1024 * 1024, // 10MB maximum file size
+    }
+}));
 
-app.use(clerkMiddleware()); // this will add auth to req obj => req.auth.userId
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
@@ -26,6 +38,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+//error handling
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message })
+})
 
 app.listen(PORT, () => {
     console.log("Server is running on port", + PORT);
