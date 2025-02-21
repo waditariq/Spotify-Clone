80ebce95b3b96122f29396090cc6b23726d52d93
waditariq <tariqalt300@gmail.com>
2025-02-21 11:59:41 -0800

    Commit #7

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,3 @@
-export const getAdmin = (req, res) => {
+export const createSong = (req, res) => {
     res.send("Admin route with the GET method");
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from '@clerk/express'
 
 import { connectDB } from './lib/db.js';
 
@@ -16,6 +17,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // to parse req.body
+
+app.use(clerkMiddleware()); // this will add auth to req obj => req.auth.userId
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
